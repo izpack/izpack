9deb2ffcd97e4bab1ee148550b674cf2a1cfa344
Ange POZZO <ange.pozzo@sage.com>
2014-03-10 16:39:00 +0100

    add dos batch file type

--- src/lib/com/izforge/izpack/util/VariableSubstitutor.java
+++ src/lib/com/izforge/izpack/util/VariableSubstitutor.java
@@ -22,9 +22,13 @@
 package com.izforge.izpack.util;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
+import com.izforge.izpack.util.os.WinConsole;
+
 
 /**
  * Substitutes variables occurring in an input stream or a string. This implementation supports a
@@ -97,6 +101,11 @@ public class VariableSubstitutor implements Serializable
     protected final static int TYPE_X3WEB = 7;
 
     /**
+     * A constant for file type. Plain file with Dos Batch variables markers
+     */
+    protected final static int TYPE_BATCH = 8;
+
+    /**
      * PLAIN = "plain"
      */
     public final static String PLAIN = "plain";
@@ -110,6 +119,8 @@ public class VariableSubstitutor implements Serializable
      * A mapping of file type names to corresponding integer constants.
      */
     protected final static Map<String, Integer> typeNameToConstantMap;
+    
+    public final static Map<Integer, String> dosCodePageIdToNameMap;
 
     // Initialize the file type map
     static
@@ -123,6 +134,162 @@ public class VariableSubstitutor implements Serializable
         typeNameToConstantMap.put("at", TYPE_AT);
         typeNameToConstantMap.put("ant", TYPE_ANT);
         typeNameToConstantMap.put("x3web", TYPE_X3WEB);
+        typeNameToConstantMap.put("batch", TYPE_BATCH);
+        
+        dosCodePageIdToNameMap = new HashMap<Integer, String>();
+        dosCodePageIdToNameMap.put(037, "IBM037");
+        dosCodePageIdToNameMap.put(437, "IBM437");
+        dosCodePageIdToNameMap.put(500, "IBM500");
+        dosCodePageIdToNameMap.put(708, "ASMO-708");
+        dosCodePageIdToNameMap.put(709, "Arabic");
+        dosCodePageIdToNameMap.put(710, "Arabic");
+        dosCodePageIdToNameMap.put(720, "DOS-720");
+        dosCodePageIdToNameMap.put(737, "ibm737");
+        dosCodePageIdToNameMap.put(775, "ibm775");
+        dosCodePageIdToNameMap.put(850, "ibm850");
+        dosCodePageIdToNameMap.put(852, "ibm852");
+        dosCodePageIdToNameMap.put(855, "IBM855");
+        dosCodePageIdToNameMap.put(857, "ibm857");
+        dosCodePageIdToNameMap.put(858, "IBM00858");
+        dosCodePageIdToNameMap.put(860, "IBM860");
+        dosCodePageIdToNameMap.put(861, "ibm861");
+        dosCodePageIdToNameMap.put(862, "DOS-862");
+        dosCodePageIdToNameMap.put(863, "IBM863");
+        dosCodePageIdToNameMap.put(864, "IBM864");
+        dosCodePageIdToNameMap.put(865, "IBM865");
+        dosCodePageIdToNameMap.put(866, "cp866");
+        dosCodePageIdToNameMap.put(869, "ibm869");
+        dosCodePageIdToNameMap.put(870, "IBM870");
+        dosCodePageIdToNameMap.put(874, "windows-874");
+        dosCodePageIdToNameMap.put(875, "cp875");
+        dosCodePageIdToNameMap.put(932, "shift_jis");
+        dosCodePageIdToNameMap.put(936, "gb2312");
+        dosCodePageIdToNameMap.put(949, "ks_c_5601-1987");
+        dosCodePageIdToNameMap.put(950, "big5");
+        dosCodePageIdToNameMap.put(1026, "IBM1026");
+        dosCodePageIdToNameMap.put(1047, "IBM01047");
+        dosCodePageIdToNameMap.put(1140, "IBM01140");
+        dosCodePageIdToNameMap.put(1141, "IBM01141");
+        dosCodePageIdToNameMap.put(1142, "IBM01142");
+        dosCodePageIdToNameMap.put(1143, "IBM01143");
+        dosCodePageIdToNameMap.put(1144, "IBM01144");
+        dosCodePageIdToNameMap.put(1145, "IBM01145");
+        dosCodePageIdToNameMap.put(1146, "IBM01146");
+        dosCodePageIdToNameMap.put(1147, "IBM01147");
+        dosCodePageIdToNameMap.put(1148, "IBM01148");
+        dosCodePageIdToNameMap.put(1149, "IBM01149");
+        dosCodePageIdToNameMap.put(1200, "utf-16");
+        dosCodePageIdToNameMap.put(1201, "unicodeFFFE");
+        dosCodePageIdToNameMap.put(1250, "windows-1250");
+        dosCodePageIdToNameMap.put(1251, "windows-1251");
+        dosCodePageIdToNameMap.put(1252, "windows-1252");
+        dosCodePageIdToNameMap.put(1253, "windows-1253");
+        dosCodePageIdToNameMap.put(1254, "windows-1254");
+        dosCodePageIdToNameMap.put(1255, "windows-1255");
+        dosCodePageIdToNameMap.put(1256, "windows-1256");
+        dosCodePageIdToNameMap.put(1257, "windows-1257");
+        dosCodePageIdToNameMap.put(1258, "windows-1258");
+        dosCodePageIdToNameMap.put(1361, "Johab");
+        dosCodePageIdToNameMap.put(10000, "macintosh");
+        dosCodePageIdToNameMap.put(10001, "x-mac-japanese");
+        dosCodePageIdToNameMap.put(10002, "x-mac-chinesetrad");
+        dosCodePageIdToNameMap.put(10003, "x-mac-korean");
+        dosCodePageIdToNameMap.put(10004, "x-mac-arabic");
+        dosCodePageIdToNameMap.put(10005, "x-mac-hebrew");
+        dosCodePageIdToNameMap.put(10006, "x-mac-greek");
+        dosCodePageIdToNameMap.put(10007, "x-mac-cyrillic");
+        dosCodePageIdToNameMap.put(10008, "x-mac-chinesesimp");
+        dosCodePageIdToNameMap.put(10010, "x-mac-romanian");
+        dosCodePageIdToNameMap.put(10017, "x-mac-ukrainian");
+        dosCodePageIdToNameMap.put(10021, "x-mac-thai");
+        dosCodePageIdToNameMap.put(10029, "x-mac-ce");
+        dosCodePageIdToNameMap.put(10079, "x-mac-icelandic");
+        dosCodePageIdToNameMap.put(10081, "x-mac-turkish");
+        dosCodePageIdToNameMap.put(10082, "x-mac-croatian");
+        dosCodePageIdToNameMap.put(12000, "utf-32");
+        dosCodePageIdToNameMap.put(12001, "utf-32BE");
+        dosCodePageIdToNameMap.put(20000, "x-Chinese_CNS");
+        dosCodePageIdToNameMap.put(20001, "x-cp20001");
+        dosCodePageIdToNameMap.put(20002, "x_Chinese-Eten");
+        dosCodePageIdToNameMap.put(20003, "x-cp20003");
+        dosCodePageIdToNameMap.put(20004, "x-cp20004");
+        dosCodePageIdToNameMap.put(20005, "x-cp20005");
+        dosCodePageIdToNameMap.put(20105, "x-IA5");
+        dosCodePageIdToNameMap.put(20106, "x-IA5-German");
+        dosCodePageIdToNameMap.put(20107, "x-IA5-Swedish");
+        dosCodePageIdToNameMap.put(20108, "x-IA5-Norwegian");
+        dosCodePageIdToNameMap.put(20127, "us-ascii");
+        dosCodePageIdToNameMap.put(20261, "x-cp20261");
+        dosCodePageIdToNameMap.put(20269, "x-cp20269");
+        dosCodePageIdToNameMap.put(20273, "IBM273");
+        dosCodePageIdToNameMap.put(20277, "IBM277");
+        dosCodePageIdToNameMap.put(20278, "IBM278");
+        dosCodePageIdToNameMap.put(20280, "IBM280");
+        dosCodePageIdToNameMap.put(20284, "IBM284");
+        dosCodePageIdToNameMap.put(20285, "IBM285");
+        dosCodePageIdToNameMap.put(20290, "IBM290");
+        dosCodePageIdToNameMap.put(20297, "IBM297");
+        dosCodePageIdToNameMap.put(20420, "IBM420");
+        dosCodePageIdToNameMap.put(20423, "IBM423");
+        dosCodePageIdToNameMap.put(20424, "IBM424");
+        dosCodePageIdToNameMap.put(20833, "x-EBCDIC-KoreanExtended");
+        dosCodePageIdToNameMap.put(20838, "IBM-Thai");
+        dosCodePageIdToNameMap.put(20866, "koi8-r");
+        dosCodePageIdToNameMap.put(20871, "IBM871");
+        dosCodePageIdToNameMap.put(20880, "IBM880");
+        dosCodePageIdToNameMap.put(20905, "IBM905");
+        dosCodePageIdToNameMap.put(20924, "IBM00924");
+        dosCodePageIdToNameMap.put(20932, "EUC-JP");
+        dosCodePageIdToNameMap.put(20936, "x-cp20936");
+        dosCodePageIdToNameMap.put(20949, "x-cp20949");
+        dosCodePageIdToNameMap.put(21025, "cp1025");
+        dosCodePageIdToNameMap.put(21027, "(deprecated)");
+        dosCodePageIdToNameMap.put(21866, "koi8-u");
+        dosCodePageIdToNameMap.put(28591, "iso-8859-1");
+        dosCodePageIdToNameMap.put(28592, "iso-8859-2");
+        dosCodePageIdToNameMap.put(28593, "iso-8859-3");
+        dosCodePageIdToNameMap.put(28594, "iso-8859-4");
+        dosCodePageIdToNameMap.put(28595, "iso-8859-5");
+        dosCodePageIdToNameMap.put(28596, "iso-8859-6");
+        dosCodePageIdToNameMap.put(28597, "iso-8859-7");
+        dosCodePageIdToNameMap.put(28598, "iso-8859-8");
+        dosCodePageIdToNameMap.put(28599, "iso-8859-9");
+        dosCodePageIdToNameMap.put(28603, "iso-8859-13");
+        dosCodePageIdToNameMap.put(28605, "iso-8859-15");
+        dosCodePageIdToNameMap.put(29001, "x-Europa");
+        dosCodePageIdToNameMap.put(38598, "iso-8859-8-i");
+        dosCodePageIdToNameMap.put(50220, "iso-2022-jp");
+        dosCodePageIdToNameMap.put(50221, "csISO2022JP");
+        dosCodePageIdToNameMap.put(50222, "iso-2022-jp");
+        dosCodePageIdToNameMap.put(50225, "iso-2022-kr");
+        dosCodePageIdToNameMap.put(50227, "x-cp50227");
+        dosCodePageIdToNameMap.put(50229, "ISO");
+        dosCodePageIdToNameMap.put(50930, "EBCDIC");
+        dosCodePageIdToNameMap.put(50931, "EBCDIC");
+        dosCodePageIdToNameMap.put(50933, "EBCDIC");
+        dosCodePageIdToNameMap.put(50935, "EBCDIC");
+        dosCodePageIdToNameMap.put(50936, "EBCDIC");
+        dosCodePageIdToNameMap.put(50937, "EBCDIC");
+        dosCodePageIdToNameMap.put(50939, "EBCDIC");
+        dosCodePageIdToNameMap.put(51932, "euc-jp");
+        dosCodePageIdToNameMap.put(51936, "EUC-CN");
+        dosCodePageIdToNameMap.put(51949, "euc-kr");
+        dosCodePageIdToNameMap.put(51950, "EUC");
+        dosCodePageIdToNameMap.put(52936, "hz-gb-2312");
+        dosCodePageIdToNameMap.put(54936, "GB18030");
+        dosCodePageIdToNameMap.put(57002, "x-iscii-de");
+        dosCodePageIdToNameMap.put(57003, "x-iscii-be");
+        dosCodePageIdToNameMap.put(57004, "x-iscii-ta");
+        dosCodePageIdToNameMap.put(57005, "x-iscii-te");
+        dosCodePageIdToNameMap.put(57006, "x-iscii-as");
+        dosCodePageIdToNameMap.put(57007, "x-iscii-or");
+        dosCodePageIdToNameMap.put(57008, "x-iscii-ka");
+        dosCodePageIdToNameMap.put(57009, "x-iscii-ma");
+        dosCodePageIdToNameMap.put(57010, "x-iscii-gu");
+        dosCodePageIdToNameMap.put(57011, "x-iscii-pa");
+        dosCodePageIdToNameMap.put(65000, "utf-7");
+        dosCodePageIdToNameMap.put(65001, "utf-8");
+        
     }
 
     /**
@@ -186,7 +353,7 @@ public class VariableSubstitutor implements Serializable
         // Return the resulting string
         return writer.getBuffer().toString();
     }
-
+    
     /**
      * Substitutes the variables found in the specified input stream. Escapes special characters
      * using file type specific escaping if necessary.
@@ -214,6 +381,9 @@ public class VariableSubstitutor implements Serializable
                     break;
                 case TYPE_XML:
                     encoding = "UTF-8";
+                    break;
+                case TYPE_BATCH:
+                    encoding = dosCodePageIdToNameMap.get(WinConsole.INSTANCE.GetConsoleOutputCP());
                     break;
             }
         }
@@ -486,7 +656,7 @@ public class VariableSubstitutor implements Serializable
             return integer;
         }
     }
-
+    
     /**
      * Escapes the special characters in the specified string using file type specific rules.
      *
@@ -508,6 +678,9 @@ public class VariableSubstitutor implements Serializable
             case TYPE_SHELL:
                 //apple mac has major problem with \r, make sure they are gone
                 return str.replace("\r","");
+            case TYPE_BATCH:
+                //return str.replace("^", "^^").replace("<", "^<").replace(">", "^>").replace("|", "^|").replace("&", "^&").replace("%", "%%");
+                return str.replace("%", "%%").replace("\\\"", "\\\\\"");
             case TYPE_JAVA_PROPERTIES:
             case TYPE_JAVA:
                 buffer = new StringBuffer(str);
