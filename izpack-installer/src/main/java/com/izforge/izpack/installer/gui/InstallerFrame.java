8d8526bf2c23f36fbf892721eeb019be0dc22e77
Miles Tjandrawidjaja <mtjandra@redhat.com>
2014-07-16 15:41:20 -0400

    - If the user's only option is to hit quit, we check to see if installer has installed something, and if yes write uninstaller. - If the user's hits quit and installation is partial, first get confirmation then write uninstaller. (Modified for this) - If the user hits quit and nothing has been installed yet (no InstallPanels past) then confirm that user wants to quit.

--- izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
@@ -673,22 +673,28 @@ public class InstallerFrame extends JFrame implements InstallerBase, InstallerVi
      */
     void quit()
     {
-        // FIXME !!! Reboot handling
-        if(!installdata.isCanClose())
+        //If fully or partially installed and only option is to quit, write uninstaller and do not prompt user about cancellation
+        if(installdata.isCanClose() && (!navigator.isNextEnabled() && !navigator.isPreviousEnabled()))
         {
-            // The installation is not over
-            confirmExit();
+            writeUninstallData();
         }
         else
         {
-            if (!writeUninstallData())
+            //Prompt user about cancellation if user has options other than quit
+            if(confirmExit())
             {
-                // TODO - for now just shut down. Alternative approaches include:
-                // . retry
-                // . revert installation - which is what wipeAborted attempts to do, but fails to handle shortcuts and
-                //                         registry changes
+                //Write uninstaller if installation is fully or partially installed
+                if (installdata.isCanClose())
+                {
+                    writeUninstallData();
+                }
+                //Just quit if nothing was installed
+                else
+                {
+                    wipeAborted();
+                    housekeeper.shutDown(0);
+                }
             }
-            shutdown();
         }
     }
 
@@ -1490,7 +1496,7 @@ public class InstallerFrame extends JFrame implements InstallerBase, InstallerVi
      *
      * @return <tt>true</tt> if uninstall data was written successfully or is not required, otherwise <tt>false</tt>
      */
-    private boolean writeUninstallData()
+    private void writeUninstallData()
     {
         boolean result = true;
         if (uninstallDataWriter.isUninstallRequired())
@@ -1502,9 +1508,13 @@ public class InstallerFrame extends JFrame implements InstallerBase, InstallerVi
                 String title = messages.get("installer.error");
                 String message = messages.get("installer.uninstall.writefailed");
                 JOptionPane.showMessageDialog(this, message, title, JOptionPane.ERROR_MESSAGE);
+                // TODO - for now just shut down. Alternative approaches include:
+                // . retry
+                // . revert installation - which is what wipeAborted attempts to do, but fails to handle shortcuts and
+                //                         registry changes
             }
         }
-        return result;
+        shutdown();
     }
 
     /**
@@ -1555,12 +1565,12 @@ public class InstallerFrame extends JFrame implements InstallerBase, InstallerVi
     /**
      * Confirms exit when installation is not complete.
      */
-    private void confirmExit()
+    private boolean confirmExit()
     {
         if (unpacker.isInterruptDisabled() && interruptCount < MAX_INTERRUPT)
         { // But we should not interrupt.
             interruptCount++;
-            return;
+            return false;
         }
 
         Messages messages = locales.getMessages();
@@ -1585,11 +1595,7 @@ public class InstallerFrame extends JFrame implements InstallerBase, InstallerVi
         title = variables.replace(title);
 
         int res = JOptionPane.showConfirmDialog(this, message, title, JOptionPane.YES_NO_OPTION);
-        if (res == JOptionPane.YES_OPTION)
-        {
-            wipeAborted();
-            housekeeper.shutDown(0);
-        }
+        return (res == JOptionPane.YES_OPTION);
     }
 
 }
