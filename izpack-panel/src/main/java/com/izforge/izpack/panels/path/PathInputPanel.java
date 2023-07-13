d0343c294b6d20ba8e001cda90fd64806ce67b5c
Frank Cornelis <info@e-contract.be>
2023-07-13 14:38:28 +0200

    [IZPACK-1738] always show overwrite existing files dialog on new installation directory

--- izpack-panel/src/main/java/com/izforge/izpack/panels/path/PathInputPanel.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/path/PathInputPanel.java
@@ -375,6 +375,11 @@ public class PathInputPanel extends IzPanel implements ActionListener
         // "The directory already exists! Are you sure you want to install here and possibly overwrite existing files?"
         // warning dialog:
         String show = getMetadata().getConfigurationOptionValue(PathInputBase.SHOWEXISTINGDIRECTORYWARNING, installData.getRules());
+        File installationInfo = new File(dir + File.separator + InstallData.INSTALLATION_INFORMATION);
+        if (!installationInfo.exists())
+        {
+           show = "true";
+        }
         if ((show == null || Boolean.getBoolean(show))  && dir.isDirectory() && dir.list().length > 0)
         {
             result = askWarningQuestion(warn, getMessage("exists_warn"),
